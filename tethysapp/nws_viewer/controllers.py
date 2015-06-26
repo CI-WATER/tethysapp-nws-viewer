d0ecf1723b13fb166e0cddfe2af56acf8276d6a4
Justin Relitz <justin.relitz@gmail.com>
2015-06-26 15:46:09 -0600

    Newest update with animations between layers

--- tethysapp/nws_viewer/controllers.py
+++ tethysapp/nws_viewer/controllers.py
@@ -18,13 +18,37 @@ def home(request):
       minZoom=2
     )  
 
-    arc_gis_layer = MVLayer(
-    	source='TileArcGISRest',
-    	options = {'url': 'http://gis.ncdc.noaa.gov/arcgis/rest/services/' + 'cdo/nexrad/MapServer',
-                'params': 'id:NEXRAD:KMTX'},
-      legend_title = 'NWS Precip Forecast',
-    	legend_extent = [-173, 17, -65, 72]
-    )
+    # arc_gis_layer_1 = MVLayer(
+    # 	source='TileArcGISRest',
+    # 	options = {'url': 'http://gis.srh.noaa.gov/arcgis/rest/services/' + 'cpc_weather_hazards/MapServer',
+    #             'LAYERS': '4'},
+    #   legend_title = 'NWS Weather Hazards',
+    # 	legend_extent = [-173, 17, -65, 72]
+    # )
+
+    # arc_gis_layer_2 = MVLayer(
+    #   source='TileArcGISRest',
+    #   options = {'url': 'http://gis.srh.noaa.gov/arcgis/rest/services/' + 'ahps_gauges/MapServer',
+    #             'LAYERS': '4'},
+    #   legend_title = 'NWS Gauges',
+    #   legend_extent = [-173, 17, -65, 72]
+    # )
+
+    # arc_gis_layer_3 = MVLayer(
+    #   source='TileArcGISRest',
+    #   options = {'url': 'http://gis.srh.noaa.gov/arcgis/rest/services/' + 'FOP/MapServer',
+    #             'LAYERS': '4'},
+    #   legend_title = 'NWS Flood Outlook',
+    #   legend_extent = [-173, 17, -65, 72]
+    # )
+
+    # arc_gis_layer_4 = MVLayer(
+    #   source='TileArcGISRest',
+    #   options = {'url': 'http://gis.srh.noaa.gov/arcgis/rest/services/' + 'QPF/MapServer',
+    #             'LAYERS': '4'},
+    #   legend_title = 'NWS Precipitation Forecast',
+    #   legend_extent = [-173, 17, -65, 72]
+    # )
 
     # Define map view options
     map_view_options = MapView(
@@ -32,7 +56,7 @@ def home(request):
       width='100%',
       controls=['ZoomSlider', 'Rotate', 'FullScreen',
                 {'MousePosition': {'projection': 'EPSG:4326'}},],
-      layers=[arc_gis_layer],
+      layers=[],
       view=view_options,
       basemap={'MapQuest': {'layer': 'osm'}},
       legend=True
---
+++ tethysapp/nws_viewer/public/js/nws-viewer.js
@@ -0,0 +1,127 @@
+// Add all layers to the map
+
+// Global variables
+var play = true;
+
+// Declare urls for the arcgis servers
+var url_hazards = 'http://gis.srh.noaa.gov/arcgis/rest/services/' + 'cpc_weather_hazards/MapServer';
+var url_gauges = 'http://gis.srh.noaa.gov/arcgis/rest/services/' + 'ahps_gauges/MapServer';
+var url_flood = 'http://gis.srh.noaa.gov/arcgis/rest/services/' + 'FOP/MapServer';
+var url_precip = 'http://gis.srh.noaa.gov/arcgis/rest/services/' + 'QPF/MapServer';
+
+// Add arc gis layer
+var hazardsLayer = new ol.layer.Tile({
+  source: new ol.source.TileArcGISRest({
+  	url: url_hazards
+  }),
+  visible: false,
+});
+
+var gaugesLayer = new ol.layer.Tile({
+  source: new ol.source.TileArcGISRest({
+    url: url_gauges
+  }),
+  visible: false,
+});
+
+var floodLayer = new ol.layer.Tile({
+  source: new ol.source.TileArcGISRest({
+    url: url_flood
+  }),
+  visible: false,
+});
+
+var precipLayer = new ol.layer.Tile({
+  source: new ol.source.TileArcGISRest({
+    url: url_precip
+  }),
+  visible: false,
+});
+
+// Function to turn layers visibility off
+function turnOffLayer(layer) {
+  return layer.setVisible(false);
+}
+
+// Function to turn layers visibility on
+function turnOnLayer(layer) {
+  return layer.setVisible(true);
+}
+
+// Function to add layers to map plot
+function addLayers() {
+  // Declare map plot
+  var map_plot = TETHYS_MAP_VIEW.getMap();
+
+  // Add all layers to map
+  map_plot.addLayer(hazardsLayer);
+  map_plot.addLayer(gaugesLayer);
+  map_plot.addLayer(floodLayer);
+  map_plot.addLayer(precipLayer);
+}
+
+// Function to turn visibility on and off with set intervals
+function animateLayersOff(layer, time) {
+  return setInterval(function() {turnOffLayer(layer)}, time);
+}
+
+// Function to turn visibility on and off with set intervals
+function animateLayersOn(layer, time) {
+  return setInterval(function() {turnOnLayer(layer)}, time);
+}
+
+function animationLoop (layers, timeStep, loop) {
+  loop = typeof loop !== 'undefined' ? loop : false;
+
+  if(!play) {
+    return;
+  }
+
+  // find length of array
+  var arrayLength = layers.length;
+  var activeLayer = null;
+
+  function advance(i) {
+    // set i to zero if it's not defined
+    i = i || 0;
+    // set active layer to false if i is not zero. For first layer
+    if (i !== 0) {
+      activeLayer.setVisible(false);
+    } else {
+      layers[arrayLength - 1].setVisible(false);
+    }
+    // set active layer to first layer in array
+    activeLayer = layers[i];
+    // set first layer to
+    activeLayer.setVisible(true);
+    
+    // if statement 
+    if (i < arrayLength - 1) {
+      i++;
+      setTimeout(function() { advance(i); }, timeStep);
+    } else if (loop) {
+      i = 0;
+      setTimeout(function() { advance(i); }, timeStep);
+    }
+  }
+  // call the function
+  advance();  
+}
+
+// Add animation with the time steps I specify
+$(document).ready(function() {
+  addLayers();
+});
+
+// define the layer array for the map
+var layerArray = [hazardsLayer, gaugesLayer, floodLayer, precipLayer];
+
+// Add animation loop function to "Start Animation" button when clicked
+$('#animateBtn button').click(function() {
+  animationLoop(layerArray, 2000, true);
+})
+
+// Click event for the stop animation button
+$('#stopAnimation button').click(function() {
+  play = false;
+})
